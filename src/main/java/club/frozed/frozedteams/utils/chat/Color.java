8709f49abaadf0da861cee3995fb2dc14c6dd856
Elb1to <elbitomc@gmail.com>
2020-05-19 16:18:16 -0500

    Added color translation utils

---
+++ src/main/java/club/frozed/frozedteams/utils/chat/Color.java
@@ -0,0 +1,19 @@
+package club.frozed.frozedteams.utils.chat;
+
+import org.bukkit.ChatColor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Color {
+
+    public static String translate(String text) {
+        String output = text;
+        return ChatColor.translateAlternateColorCodes('&', output);
+    }
+
+    public static List<String> translate(List<String> list) {
+        return list.stream().map(Color::translate).collect(Collectors.toList());
+    }
+
+}
