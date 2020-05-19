74c419ada7e999d154011e5fc90419237b2847a8
Elb1to <elbitomc@gmail.com>
2020-05-19 18:54:49 -0500

    Update Messages.java

--- src/main/java/club/frozed/frozedteams/utils/chat/Messages.java
+++ src/main/java/club/frozed/frozedteams/utils/chat/Messages.java
@@ -1,4 +1,14 @@
 package club.frozed.frozedteams.utils.chat;
 
+import club.frozed.frozedteams.FrozedTeams;
+
 public class Messages {
+
+    public static String CATCH_SUCCEEDED;
+    public static String CATCH_FAILED;
+
+    static {
+        CATCH_SUCCEEDED = Color.translate(FrozedTeams.getInstance().getConfiguration("messages.yml").getString(""));
+        CATCH_FAILED = Color.translate(FrozedTeams.getInstance().getConfiguration("messages.yml").getString(""));
+    }
 }
