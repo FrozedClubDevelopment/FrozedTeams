1c2b2061fcc3d7a8ab99891d7a83d938fee8ceef
Elb1to <elbitomc@gmail.com>
2020-05-22 03:13:20 -0500

    Registered listeners, and commands

--- src/main/java/club/frozed/frozedteams/FrozedTeams.java
+++ src/main/java/club/frozed/frozedteams/FrozedTeams.java
@@ -1,12 +1,16 @@
 package club.frozed.frozedteams;
 
+import club.frozed.frozedteams.commands.TrackCommand;
+import club.frozed.frozedteams.listeners.MobCatchingListener;
+import club.frozed.frozedteams.listeners.MobListener;
+import club.frozed.frozedteams.listeners.PlayerListener;
+import club.frozed.frozedteams.listeners.SalvagingListener;
 import club.frozed.frozedteams.utils.chat.Color;
 import club.frozed.frozedteams.utils.command.CommandFramework;
 import club.frozed.frozedteams.utils.configurations.ConfigFile;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
@@ -31,34 +35,33 @@ public class FrozedTeams extends JavaPlugin {
         // Register config files
         registerConfigurations();
 
-        if (!this.getDescription().getAuthors().contains("FrozedDevelopment") ||
-                !this.getDescription().getAuthors().contains("Elb1to") ||
-                !this.getDescription().getDescription().equals("MineHQ MCTeams replica by Elb1to") ||
-                !this.getDescription().getName().equals("FrozedTeams")) {
-            int i;
-            for (i = 0; i < 10; i++) {
-                Bukkit.getServer().broadcastMessage(ChatColor.RED + "Why are you changing the");
-                Bukkit.getServer().broadcastMessage(ChatColor.RED + "plugin yml ( ͡° ͜ʖ ͡°)╭∩╮");
-            }
+        if (!this.getDescription().getAuthors().contains("Elb1to") || !this.getDescription().getAuthors().contains("FrozedDevelopment") ||
+            !this.getDescription().getDescription().equals("MineHQ MCTeams replica by Elb1to") || !this.getDescription().getName().equals("FrozedTeams")) {
             System.exit(0);
             Bukkit.shutdown();
         }
 
         // Register Listeners
+        Bukkit.getPluginManager().registerEvents(new MobCatchingListener(), this);
+        Bukkit.getPluginManager().registerEvents(new SalvagingListener(), this);
+        Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
+        Bukkit.getPluginManager().registerEvents(new MobListener(), this);
 
         // Register Managers
 
         // Register Commands
+        this.getCommand("track").setExecutor(new TrackCommand());
 
         Bukkit.getConsoleSender().sendMessage(Color.translate("&7&m--------------------------------------------------------------"));
-        Bukkit.getConsoleSender().sendMessage(Color.translate("&7This server is using &bFrozedSG"));
+        Bukkit.getConsoleSender().sendMessage(Color.translate("&7This server is using &bFrozedTeams"));
         Bukkit.getConsoleSender().sendMessage(Color.translate("&7Authors&8: &b" + getDescription().getAuthors()));
         Bukkit.getConsoleSender().sendMessage(Color.translate("&7Version&8: &b" + getDescription().getVersion()));
         Bukkit.getConsoleSender().sendMessage(Color.translate("&7&m--------------------------------------------------------------"));
         Bukkit.getConsoleSender().sendMessage(" ");
-        Bukkit.getConsoleSender().sendMessage(Color.translate("&eChecking your spigot version..."));
-        Bukkit.getConsoleSender().sendMessage(Color.translate("&aSuccess! &eYour Server NMS version: " + getNmsVersion()));
-
+        Bukkit.getConsoleSender().sendMessage(Color.translate("&bChecking your spigot version..."));
+        Bukkit.getConsoleSender().sendMessage(Color.translate("&aSuccess! &bYour Server NMS version: " + getNmsVersion()));
+        Bukkit.getConsoleSender().sendMessage(" ");
+        Bukkit.getConsoleSender().sendMessage(Color.translate("&7&m--------------------------------------------------------------"));
     }
 
     private String getNmsVersion() {
@@ -70,15 +73,6 @@ public class FrozedTeams extends JavaPlugin {
     }
 
     public void registerConfigurations() {
-        files.addAll(Arrays.asList(
-                new ConfigFile("config"),
-                new ConfigFile("messages"),
-                new ConfigFile("items"),
-                new ConfigFile("inventory"),
-                new ConfigFile("chests"),
-                new ConfigFile("scoreboard"),
-                new ConfigFile("tablist")
-        ));
+        files.addAll(Arrays.asList(new ConfigFile("config"), new ConfigFile("messages"), new ConfigFile("design")));
     }
-
 }
