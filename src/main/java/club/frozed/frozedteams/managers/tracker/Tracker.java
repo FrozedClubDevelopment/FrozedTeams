e93fa8ae1efb30c33a0a8503fad184bd9e755f25
Elb1to <elbitomc@gmail.com>
2020-05-22 03:17:48 -0500

    Refactored class name

--- src/main/java/club/frozed/frozedteams/managers/tracker/Tracker.java
+++ src/main/java/club/frozed/frozedteams/managers/tracker/Tracker.java
@@ -2,7 +2,7 @@ package club.frozed.frozedteams.managers.tracker;
 
 import org.bukkit.entity.Player;
 
-public interface Track {
+public interface Tracker {
     void trackPlayer(Player player);
 
     void trackAll();
