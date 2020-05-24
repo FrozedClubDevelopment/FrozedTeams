7a090423efdac5c96aab0127fa5636fa974065e8
Elb1to <elbitomc@gmail.com>
2020-05-24 17:59:15 -0500

    Added Scoreboard classes

---
+++ src/main/java/club/frozed/frozedteams/utils/board/BoardAdapter.java
@@ -0,0 +1,19 @@
+package club.frozed.frozedteams.utils.board;
+
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Scoreboard;
+
+import java.util.List;
+
+public interface BoardAdapter {
+
+    String getTitle(Player player);
+
+    List<String> getScoreboard(Player player, Board board);
+
+    long getInterval();
+
+    void onScoreboardCreate(Player player, Scoreboard board);
+
+    void preLoop();
+}
