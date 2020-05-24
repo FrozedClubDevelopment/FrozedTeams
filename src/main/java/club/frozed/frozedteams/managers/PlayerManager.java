8286d3840466808cd04a743ebe231b56dee89e3b
Elb1to <elbitomc@gmail.com>
2020-05-24 18:00:27 -0500

    Create PlayerManager.java

---
+++ src/main/java/club/frozed/frozedteams/managers/PlayerManager.java
@@ -0,0 +1,13 @@
+package club.frozed.frozedteams.managers;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+public class PlayerManager {
+    @Getter public static PlayerManager instance;
+    public PlayerManager() {
+        instance = this;
+    }
+
+}
