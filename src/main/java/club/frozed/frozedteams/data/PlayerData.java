2daf61d5f05be931c5a7ee59d202e0509869345f
Elb1to <elbitomc@gmail.com>
2020-05-24 18:15:57 -0500

    Update PlayerData.java

--- src/main/java/club/frozed/frozedteams/data/PlayerData.java
+++ src/main/java/club/frozed/frozedteams/data/PlayerData.java
@@ -2,6 +2,7 @@ package club.frozed.frozedteams.data;
 
 import club.frozed.frozedteams.enums.PlayerState;
 import lombok.Data;
+import org.bukkit.Bukkit;
 
 import java.util.UUID;
 
@@ -10,6 +11,12 @@ public class PlayerData {
     private PlayerState state = PlayerState.IN_SPAWN;
     private UUID uuid;
     private String name;
+    private Stat balance = new Stat();
     //private Cooldown combatCooldown;
+
+    public PlayerData(UUID uuid) {
+        this.uuid = uuid;
+        this.name = Bukkit.getOfflinePlayer(uuid).getName();
+    }
 
 }
